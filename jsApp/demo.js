23e22ac28e36fd1d4f7ba82901c56fd964f0556f
imosquera <imosquera@tuenti.com>
2014-12-02 14:36:54 +0100

    jsApp: the answer, same js for caller and callee, you need to change the isCaller flag for each tab, notice that receiveAnswer not done yet

--- jsApp/demo.js
+++ jsApp/demo.js
@@ -1,25 +1,71 @@
+isCaller = true;
+
 error = function (error) {
     console.log(error);
-};
+}
 
-pc = new webkitRTCPeerConnection({
-    "iceServers": []
-});
-
-navigator.webkitGetUserMedia({
-    video: true,
-    audio: true
-},
-
-function (localMediaStream) {
-    pc.addStream(localMediaStream);
-    pc.createOffer(function (offer) {
-        pc.setLocalDescription(offer, function () {
-            console.log(offer.sdp);
-            console.log("Paste on callee:");
-            console.log("receiveOffer(atob('" + btoa(offer.sdp) + "'))");
-        });
+if (isCaller) {
+    pc = new webkitRTCPeerConnection({
+        "iceServers": []
     });
-},
 
-error);
+    navigator.webkitGetUserMedia({
+        video: true,
+        audio: true
+    },
+
+    function (localMediaStream) {
+        pc.addStream(localMediaStream);
+        pc.createOffer(function (offer) {
+            pc.setLocalDescription(
+            offer,
+
+            function () {
+                console.log(offer.sdp);
+                console.log("Paste on callee:");
+                console.log("receiveOffer(atob('" + btoa(offer.sdp) + "'))");
+            });
+        });
+    },
+    error
+
+    );
+}
+
+receiveOffer = function (offerSdp) {
+    var pc = new webkitRTCPeerConnection({
+        "iceServers": []
+    });
+
+    navigator.webkitGetUserMedia({
+        video: true,
+        audio: true
+    },
+
+    function (localMediaStream) {
+        pc.addStream(localMediaStream);
+        pc.setRemoteDescription(new RTCSessionDescription({
+            type: "offer",
+            sdp: offerSdp
+        }),
+
+        function () {
+            pc.createAnswer(function (answer) {
+                pc.setLocalDescription(answer);
+                console.log(answer.sdp);
+                console.log("Paste on caller:");
+                console.log("receiveAnswer(atob('" + btoa(answer.sdp) + "'))");
+            },
+            error, {
+                mandatory: {
+                    OfferToReceiveAudio: true,
+                    OfferToReceiveVideo: true
+                }
+            });
+
+        },
+        error);
+    },
+
+    error);
+}
